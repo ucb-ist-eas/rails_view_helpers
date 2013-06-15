d5c5266e44923b20025139f8774026c198138f69
Steve Downey <steve.downtown@gmail.com>
2013-06-15 14:04:24 -0700

    fix NilClass hack

--- lib/generators/rails_view_helpers/templates/config/initializers/local/core-extensions/nil_class/datetime_to_s.rb
+++ lib/generators/rails_view_helpers/templates/config/initializers/local/core-extensions/nil_class/datetime_to_s.rb
@@ -1,21 +1,22 @@
 class NilClass
   
+  alias_method :original_to_s, :to_s
+  
   # This hack enables us to call +to_s+ on a (supposedly) Date/Datetime
   # with out worrying that the value may be +nil+.
   #
   # @return [String]
   def to_s(*args)
-    return super unless args.length == 1
-    
-    format = args.first
-    return super unless known_formats.include?(format)
-    
-    return ''
+    if known_formats.include?(args.first)
+      ''
+    else
+      original_to_s(*args)
+    end
   end
   
   private
   
   def known_formats
-    @known_formats ||= Date::DATE_FORMATS + Time::DATE_FORMATS
+    @known_formats ||= Date::DATE_FORMATS.keys + Time::DATE_FORMATS.keys
   end
 end
